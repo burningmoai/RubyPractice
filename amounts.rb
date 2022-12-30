3e8615ed1c605e2ca6bd2336decd4dedefe91d0d
burningmoai <114802516+burningmoai@users.noreply.github.com>
2022-12-30 14:51:43 +0000

    [Start]6章演習

---
+++ amounts.rb
@@ -0,0 +1,4 @@
+amounts = {"オレンジ" =>2, "いちご" =>5, "リンゴ" => 3, "オムライス" => 5, "餃子" => 3}
+amounts.each do |fruit, amount|
+  puts "#{fruit}は#{amount}個です"
+end
---
+++ dice.rb
@@ -0,0 +1,6 @@
+dice = 0
+
+while dice != 3 do
+  dice = rand(1..3)
+  puts dice
+end
